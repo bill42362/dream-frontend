b1b7c3932036c4ed96b9a45fdb7593fca2890a89
Bill42362 <bill42362@gmail.com>
2016-11-16 17:28:07 +0800

    Modify ProjectItem layout to conform designer.

--- js/reception/project/react/ProjectItem.react.js
+++ js/reception/project/react/ProjectItem.react.js
@@ -12,10 +12,10 @@ class ProjectItem extends React.Component {
     componentDidUpdate() {}
     render() {
         let item = {
-            title: 'item title',
+            title: '贊助主題贊助主題贊助主題贊助主題贊助主題',
             price: 500,
             sponsorCount: 10,
-            content: 'item content item content item content item content item content item content ',
+            content: '贊助內容贊助內容贊助內容贊助內容贊助內容贊助內容贊助內容贊助內容贊助內容贊助內容贊助內容贊助內容贊助內容',
             labels: ['限量 100 組', 'label2', 'label3'],
             media: {
                 type: 'image', title: '新城國小',
@@ -33,6 +33,10 @@ class ProjectItem extends React.Component {
             )}</div>}
             <h4 className='project-item-title'>{item.title}</h4>
             <p className='project-item-content' dangerouslySetInnerHTML={{__html: item.content}}></p>
+            <div className='project-item-fake-button'>
+                <span className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+                立即贊助專案
+            </div>
         </div>;
     }
 }
