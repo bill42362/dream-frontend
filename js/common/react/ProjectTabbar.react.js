58413ab600dc8f554b415c26bd2105d5918f4598
Bill42362 <bill42362@gmail.com>
2016-11-22 16:03:41 +0800

    Done redirection function of ProjectTabbar.

--- js/common/react/ProjectTabbar.react.js
+++ js/common/react/ProjectTabbar.react.js
@@ -11,9 +11,9 @@ class ProjectTabbar extends React.Component {
     }
     render() {
         let tabs = [
-            {key: 'story', display: '專案故事', count: 0},
-            {key: 'timeline', display: '專案進度', count: 2},
-            {key: 'comment', display: '訊息回應', count: 3},
+            {key: 'story', display: '專案故事', count: 0, href: '/project.html'},
+            {key: 'timeline', display: '專案進度', count: 2, href: '#'},
+            {key: 'comment', display: '訊息回應', count: 3, href: '/project-message.html'},
         ];
         let sharingIcons = [
             {
@@ -40,13 +40,12 @@ class ProjectTabbar extends React.Component {
         return <div className='project-tabbar row' ref='base'>
             <ul className='project-tabbar-tabs col-md-8'>
                 {tabs.map((tab, index) =>
-                    <li
-                        className={ClassNames('project-tabbar-tab', {'active': 0 === index})}
-                        key={index}
-                    >
-                        {tab.display}
-                        {!!tab.count && <div className='project-tabbar-tab-count'>{tab.count}</div>}
-                    </li>
+                    <a title={tab.display} href={tab.href} key={index}>
+                        <li className={ClassNames('project-tabbar-tab', {'active': location.pathname === tab.href})} >
+                            {tab.display}
+                            {!!tab.count && <div className='project-tabbar-tab-count'>{tab.count}</div>}
+                        </li>
+                    </a>
                 )}
             </ul>
             <div className='project-tabbar-aside col-md-4'>
