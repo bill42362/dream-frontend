c1180fd47e2410f3a2c3c16a029955200a8bf848
Bill42362 <bill42362@gmail.com>
2016-12-14 11:35:40 +0800

    Done docking to api for templates.

--- js/reception/project-timeline/react/App.react.js
+++ js/reception/project-timeline/react/App.react.js
@@ -13,13 +13,60 @@ class App extends React.Component {
         super(props);
         this.staticStrings = { };
         this.state = {
-            isTabbarAboveScreen: false,
+            project: {}, stories: [], items: [], timelineItems: [], comments: [],
+            pictures: {}, isTabbarAboveScreen: false
         };
         this.onWindowScroll = this.onWindowScroll.bind(this);
+        this.onGetProjectSuccess = this.onGetProjectSuccess.bind(this);
+        let searches = Core.getUrlSearches();
+        if(!!searches.p && window.PBPlusDream) {
+            PBPlusDream.getProject(searches.p, this.onAjaxError, this.onGetProjectSuccess);
+        }
     }
     isAboveScreenTop(element, offset) {
         var clientRect = element.getBoundingClientRect();
         return 0 > clientRect.top + offset;
+    }
+    cachePicture(pictureData) {
+        var pictures = this.state.pictures;
+        if(pictureData.id) { pictures[pictureData.id] = pictureData; }
+        this.setState({pictures: pictures});
+    }
+    onGetProjectSuccess(response) {
+        if(200 === response.status) {
+            var project = response.message[0];
+            this.cachePicture(project.bannerData);
+            response.stories.forEach(function(story) {
+                this.cachePicture(story.pictureData);
+            }.bind(this));
+            response.items.forEach(function(item) {
+                this.cachePicture(item.pictureData);
+            }.bind(this));
+            response.timelineItems.forEach(function(timelineItem) {
+                this.cachePicture(timelineItem.pictureData);
+            }.bind(this));
+            this.setState({
+                project: project,
+                stories: response.stories,
+                items: response.items,
+                timelineItems: response.timelineItems,
+            });
+        } else {
+            this.onAjaxError(response);
+        }
+    }
+    onAjaxError(xhr) {
+        let networkError = '網路錯誤，請檢查您的網路，或稍候再試一次。<br />'
+            + 'Network error, please check your network, or try again later.';
+        let systemError = '系統錯誤，請稍候再試一次。<br />System error, please try again later.';
+        var errorStrings = this.staticStrings.errors;
+        if(!xhr.message) {
+            Toastr['error'](networkError);
+        } else if(/[45]\d\d/.test(xhr.status)) {
+            Toastr['error'](xhr.status + ' "' + xhr.message + '"<br />' + systemError);
+        } else {
+            Toastr['warning'](xhr.status + xhr.message);
+        }
     }
     onWindowScroll(e) {
         var isTabbarAboveScreen = this.state.isTabbarAboveScreen;
@@ -29,6 +76,23 @@ class App extends React.Component {
     componentDidMount() { document.addEventListener('scroll', this.onWindowScroll, false); }
     componentWillUnmount() { document.removeEventListener('scroll', this.onWindowScroll, false); }
     render() {
+        const state = this.state;
+        let tabs = [];
+        tabs.push(
+            {key: 'story', display: '專案故事', count: 0, href: '/project?p=' + state.project.id}
+        );
+        if(state.timelineItems.length) {
+            tabs.push({
+                key: 'timeline', display: '專案進度',
+                count: state.timelineItems.length, href: '/timeline?p=' + state.project.id
+            });
+        }
+        if(state.comments.length) {
+            tabs.push({
+                key: 'comment', display: '訊息回應',
+                count: state.comments.length, href: '/message?p=' + state.project.id
+            });
+        }
         let proposer = {title: 'pb+寶悍運動平台', href: '//www.pbplus.me/'};
         let banner = {
             type: 'image', title: '新城國小',
@@ -71,7 +135,7 @@ class App extends React.Component {
         return <div id='wrapper'>
             <Header fixed={false} />
             <ProjectHeader
-                projectData={projectFullData} proposer={proposer} banner={banner}
+                project={this.state.project} banner={this.state.pictures[this.state.project.bannerId]}
             />
             <div
                 ref='projectTabbarContainerAnchor'
@@ -81,7 +145,7 @@ class App extends React.Component {
                 )}
             >
                 <div className='project-tabbar-container' >
-                    <ProjectTabbar />
+                    <ProjectTabbar tabs={tabs} />
                 </div>
             </div>
             <div className='project-content-container'>
