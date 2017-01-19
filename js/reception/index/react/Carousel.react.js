a7303d8f4fbd95cdf2a4dbfa26af0115cf0ff2bf
Bill42362 <bill42362@gmail.com>
2017-01-19 12:07:47 +0800

    Enable display mock user icons with texts.

--- js/reception/index/react/Carousel.react.js
+++ js/reception/index/react/Carousel.react.js
@@ -5,13 +5,22 @@ import ClassNames from 'classnames';
 class Carousel extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { };
+        const mockIconPool = [
+            '山羌', '鳳蝶', '黑熊', '石虎', '獼猴',
+            '蛇蜥', '眼蝶', '野豬', '麝鼩', '森鼠',
+            '刺鼠', '狐蝠', '雲豹', '八哥', '山雀',
+            '雲雀', '卷尾', '藪眉', '畫眉', '草蜥',
+        ];
+        this.state = {
+            mockIcons: mockIconPool.sort(() => { return Math.floor(2*Math.random()) - 1; }),
+        };
     }
     render() {
+        const { mockIcons } = this.state;
         const { newsfeeds, userProfiles } = this.props;
         let carouselItems = newsfeeds.map(newsfeed => {
             let nickname = 'pb+ 會員';
-            let imageSrc = '/img/mock_user_icon.jpg';
+            let imageSrc = '';
             if(userProfiles[newsfeed.userPK]) {
                 nickname = userProfiles[newsfeed.userPK].nickname || nickname;
                 imageSrc = userProfiles[newsfeed.userPK].src || imageSrc;
@@ -26,7 +35,15 @@ class Carousel extends React.Component {
             {carouselItems.map((item, index) =>
                 <div className='carousel-item' key={index}>
                     <div className='carousel-item-image-wrapper'>
-                        <img className='carousel-item-image' src={item.imageSrc} />
+                        {item.imageSrc && <img className='carousel-item-image' src={item.imageSrc} />}
+                        {!item.imageSrc && <div
+                            className='carousel-mock-image'
+                            style={{
+                                width: '100%', height: '100%',
+                                color: 'darkorchid', fontWeight: '600',
+                                textAlign: 'center', lineHeight: '4em'
+                            }}
+                        >{mockIcons[index]}</div>}
                     </div>
                     <div className='carousel-item-text'>{`${item.text} ..................`}</div>
                 </div>
