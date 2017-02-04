403e5f85d4a8e683a13d309a1cf766322e520260
Bill42362 <bill42362@gmail.com>
2017-02-05 01:43:29 +0800

    Add `checkMouseOnEdge()` in ImageInputBox and enable dynamic mouse cursor.

--- js/reception/user-info/react/ImageInputBox.react.js
+++ js/reception/user-info/react/ImageInputBox.react.js
@@ -7,7 +7,27 @@ import MouseTracker from './MouseTracker.react.js';
 class ImageInputBox extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {mouseCursor: 'move'};
         this.onDrag = this.onDrag.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+    }
+    checkMouseOnEdge({ x, y }, element) {
+        const offset = 10;
+        const { width, height } = element.getBoundingClientRect();
+        return {
+            isOnLeft: offset > x, isOnRight: offset > width - x,
+            isOnTop: offset > y, isOnBottom: offset > height - y,
+        };
+    }
+    onMouseMove(mouseState) {
+        const { isOnLeft, isOnRight, isOnTop, isOnBottom }
+            = this.checkMouseOnEdge(mouseState.axis, this.refs.fullViewContainer);
+        let mouseCursor = 'move';
+        if(isOnLeft || isOnRight) { mouseCursor = 'ew-resize'; }
+        if(isOnTop || isOnBottom) { mouseCursor = 'ns-resize'; }
+        if((isOnLeft && isOnTop) || (isOnRight && isOnBottom)) { mouseCursor = 'nwse-resize'; }
+        else if((isOnLeft && isOnBottom) || (isOnRight && isOnTop)) { mouseCursor = 'nesw-resize'; }
+        if(mouseCursor != this.state.mouseCursor) { this.setState({ mouseCursor }); }
     }
     onDrag(mouseState) {
         const { movePicture } = this.props;
@@ -15,6 +35,7 @@ class ImageInputBox extends React.Component {
         movePicture && movePicture({x: 0.5*move.x, y: 0.5*move.y});
     }
     render() {
+        const { mouseCursor } = this.state;
         const { editorState, source, movePicture, style } = this.props;
         const { top, left, width, height } = editorState;
         return <div
@@ -23,16 +44,18 @@ class ImageInputBox extends React.Component {
         >
             <img style={{borderRadius: 60}} src={editorState.resultSource} />
             <div
+                ref='fullViewContainer'
                 style={{
                     position: 'absolute', opacity: 0.5,
+                    border: '2px dashed darkgray',
                     width: editorState.image.width,
                     height: editorState.image.height,
-                    cursor: 'move',
+                    cursor: mouseCursor,
                     top, left
                 }}
             >
                 <img style={{ width: '100%', height: '100%' }} src={editorState.image.src} />
-                <MouseTracker onMouseDrag={this.onDrag} />
+                <MouseTracker onMouseDrag={this.onDrag} onMouseMove={this.onMouseMove} />
             </div>
             <div className='edit-button' role='button'>
                 <span className='glyphicon glyphicon-camera'></span>
