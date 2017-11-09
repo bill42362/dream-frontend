ed295a56995950a85180076f6a700c4000b02888
Bill42362 <bill42362@gmail.com>
2017-11-09 16:47:04 +0800

    Dock project-message.html to `pbplus-member-sdk` but message api broken.

--- js/reception/project-message/core/App.js
+++ js/reception/project-message/core/App.js
@@ -2,8 +2,10 @@
 'use strict'
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-import { Provider } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import PbplusMemberCenter from 'pbplus-member-sdk';
+import Auth from '../../../common/core/Auth.js';
 import App from '../react/App.react.js';
 import Core from '../../../common/core/Core.js';
 import Dream from '../../../common/core/Dream.js';
@@ -13,6 +15,8 @@ import Navigations from '../../../common/core/Navigations.js';
 window.PBPlusDream = new Dream();
 
 const reducer = combineReducers({
+    pbplusMemberCenter: PbplusMemberCenter.Reducer,
+    auth: Auth.Reducer,
     navigations: Navigations.Reducer,
     sitemap: Sitemap.Reducer,
 })
@@ -26,13 +30,33 @@ PBPlusDream.getHeaderNavs()
 })
 .catch(error => { console.log(error); });
 
-var onReactDOMRendered = function() { }
+store.dispatch(Auth.Actions.fetchLoginState())
+.then(({ isUserLoggedIn }) => {
+    if(isUserLoggedIn) {
+        return store.dispatch(PbplusMemberCenter.Actions.updateActiveTab({activeTab: 'personal-data'}));
+    }
+    return {isUserLoggedIn: false};
+})
+.catch(error => { console.log(error); });
 
+const ConnectedApp = connect(
+    state => {
+        const { nickname } = state.pbplusMemberCenter.personalData;
+        const { resultSource: picture } = state.pbplusMemberCenter.pictureEditor;
+        return {
+            isUserLoggedIn: state.auth.isUserLoggedIn,
+            user: { nickname, picture },
+        };
+    },
+    dispatch => ({})
+)(App);
+
+var onReactDOMRendered = function() { }
 var onReadyStateChange = function() {
     if(document.readyState == 'complete') {
         ReactDOM.render(
             <Provider store={store} >
-                <App />
+                <ConnectedApp />
             </Provider>,
             document.getElementById('app-root'),
             onReactDOMRendered
