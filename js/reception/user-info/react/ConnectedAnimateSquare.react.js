dc3a4054195a59f38ab14208b036fb505eb5cacc
Bill42362 <bill42362@gmail.com>
2017-11-20 14:59:19 +0800

    Remove user-info.html and remove unneeded bootstrap components.

--- js/reception/user-info/react/ConnectedAnimateSquare.react.js
+++
@@ -1,7 +0,0 @@
-// ConnectedAnimateSquare.react.js
-'use strict'
-import { connect } from 'react-redux';
-import { Component } from 'animate-square';
-
-const mapStateToProps = (state) => { return {squares: state.animateSquare}; };
-export default connect(mapStateToProps)(Component);
